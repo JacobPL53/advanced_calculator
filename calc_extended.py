482fd4ac4a1edcc768f76ec8532d74c6031b2cd2
JacobPL53 <110395097+JacobPL53@users.noreply.github.com>
2022-08-01 14:05:22 +0200

    Update calc_extended.py

--- calc_extended.py
+++ calc_extended.py
@@ -9,4 +9,4 @@ elif(Category == 3):
     Patterns = ["ByteCalculator"]
 else:
     print("Type correct number")
-print("Select one of those: {0}".format(Patterns))
+print("Select one of those: \n 1 - {}\n 2 - {}\n 3 - {}\n 4 - {}\n 5 - {}\n 6 - {}\n 7 - {}\n 8 - {}\n 9 - {}\n10 - {}\n11 - {}\n12 - {}\n13 - {}\n14 - {}\n15 - {}\n16 - {}\n17 - {}\n18 - {}\n19 - {}\n20 - {}\n21 - {}".format(Patterns[0],Patterns[1],Patterns[2],Patterns[3],Patterns[4],Patterns[5],Patterns[6],Patterns[7],Patterns[8],Patterns[9],Patterns[10],Patterns[11],Patterns[12],Patterns[13],Patterns[14],Patterns[15],Patterns[16],Patterns[17],Patterns[18],Patterns[19],Patterns[20]))
