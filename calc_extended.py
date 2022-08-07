ab0bc1ab00d79f144354c8bd69e3448965193026
JacobPL53 <110395097+JacobPL53@users.noreply.github.com>
2022-08-07 13:12:02 +0200

    Add files via upload

--- calc_extended.py
+++ calc_extended.py
@@ -1,3 +1,4 @@
+from cmath import pi
 import math
 #Creating global variables: 'DirectPattern' - String and 'Patterns' - Array
 DirectPattern = ""
@@ -8,18 +9,18 @@ def TriangleCalc():
     if(Triangle == "e" or Triangle == "E"):
         SideLength = int(input("Type side length of your triangle: "))
         TriCirc = SideLength * 3
-        print("Circuit of your triangle is: {}".format(TriCirc))
+        print("Circumference of your triangle is: {}".format(TriCirc))
     elif(Triangle == "i" or Triangle == "I"):
         TriLeg = int(input("Type legs length in your triangle: "))
         TriBase = int(input("Type base length in your triangle: "))
         TriCirc = TriLeg * 2 + TriBase
-        print("Circuit of your triangle is: {}".format(TriCirc))
+        print("Circumference of your triangle is: {}".format(TriCirc))
     elif(Triangle == "d" or Triangle == "D"):
         TriSide1 = int(input("Type first side length in your triangle: "))
         TriSide2 = int(input("Type secord side length in your triangle: "))
         TriSide3 = int(input("Type third side length in your triangle: "))
         TriCirc = TriSide1 + TriSide2 + TriSide3
-        print("Circuit of your triangle is: {}".format(TriCirc))
+        print("Circumference of your triangle is: {}".format(TriCirc))
     else:
         print("Type correct letter: e or i or d")
         TriangleCalc()
@@ -38,6 +39,24 @@ def RhombusCalc():
     else:
         print("Type correct letter: d or s")
         RhombusCalc()
+def TrapezeCalc():
+    IsIsosceles = input("Is your trapeze isosceles or not? (y/n): ")
+    if(IsIsosceles == "y" or IsIsosceles == "Y"):
+        BaseLength1 = int(input("Type first base length of your trapeze: "))
+        BaseLength2 = int(input("Type second base length of your trapeze: "))
+        TrapLeg = int(input("Type legs length in your trapeze: "))
+        TrapCirc = BaseLength1 + BaseLength2 + TrapLeg * 2
+    elif(IsIsosceles == "n" or IsIsosceles == "N"):
+        BaseLength1 = int(input("Type first base length of your trapeze: "))
+        BaseLength2 = int(input("Type second base length of your trapeze: "))
+        TrapLeg1 = int(input("Type first leg length in your trapeze: "))
+        TrapLeg2 = int(input("Type second leg length in your trapeze: "))
+        TrapCirc = BaseLength1 + BaseLength2 + TrapLeg1 + TrapLeg2
+    else:
+        print("Type correct letter: y or n")
+        TrapezeCalc()
+    print("Circumference of your trapeze is: {}".format(TrapCirc))
+    
 def Calc():
     branchOfScience = int(input("Enter branch of your question: \nType 1 for Maths\nType 2 for Chemistry\nType 3 for Informatics\n:"))
     if(branchOfScience == 1):
@@ -48,8 +67,8 @@ def Calc():
             print("Basic Calculator isn't available now.")
             Calc()
         elif(Category == 2):
-            Patterns = ["Square Diagonal","Square Area","Square Circuit","Rectangle Area","Rectangle Circuit","Triangle Area","Triangle Circuit","Rhombus Area","Rhombus Circuit","Parallelogram Area","Parallelogram Circuit","Trapeze Area","Trapeze Circuit","Sphere Area","Sphere Circuit","Cube Area","Cube Volume","Cuboid Area","Cuboid Volume","Cylinder Area","Cylinder Volume"]
-            print("\n 1 - {}\n 2 - {}\n 3 - {}\n 4 - {}\n 5 - {}\n 6 - {}\n 7 - {}\n 8 - {}\n 9 - {}\n10 - {}\n11 - {}\n12 - {}\n13 - {}\n14 - {}\n15 - {}\n16 - {}\n17 - {}\n18 - {}\n19 - {}\n20 - {}\n21 - {}\n".format(*Patterns))
+            Patterns = ["Square Diagonal","Square Area","Square Circumference","Rectangle Area","Rectangle Circumference","Triangle Area","Triangle Circumference","Rhombus Area","Rhombus Circumference","Parallelogram Area","Parallelogram Circumference","Trapeze Area","Trapeze Circumference","Circle Area","Circle Circumference","Cube Area","Cube Volume","Cuboid Area","Cuboid Volume","Cylinder Area","Cylinder Volume","Sphere Area","Sphere Volume"]
+            print("\n 1 - {}\n 2 - {}\n 3 - {}\n 4 - {}\n 5 - {}\n 6 - {}\n 7 - {}\n 8 - {}\n 9 - {}\n10 - {}\n11 - {}\n12 - {}\n13 - {}\n14 - {}\n15 - {}\n16 - {}\n17 - {}\n18 - {}\n19 - {}\n20 - {}\n21 - {}\n22 - {}\n23 - {}\n".format(*Patterns))
             DirectPattern = int(input("Select one of above: "))
             if(DirectPattern == 1):
                 SideLength = int(input("Type side length of your square: "))
@@ -62,7 +81,7 @@ def Calc():
             elif(DirectPattern == 3):
                 SideLength = int(input("Type side length of your square: "))
                 SquareCirc = SideLength * 4
-                print("Circuit of your square is: {}".format(SquareCirc))
+                print("Circumference of your square is: {}".format(SquareCirc))
             elif(DirectPattern == 4):
                 RectWidth = int(input("Type width of your rectangle: "))
                 RectHeight = int(input("Type height of your rectangle: "))
@@ -72,7 +91,7 @@ def Calc():
                 RectWidth = int(input("Type width of your rectangle: "))
                 RectHeight = int(input("Type height of your rectangle: "))
                 RectCirc = 2 * RectWidth + 2 * RectHeight
-                print("Circuit of your rectangle is: {}".format(RectCirc))
+                print("Circumference of your rectangle is: {}".format(RectCirc))
             elif(DirectPattern == 6):
                 SideLength = int(input("Type side length of your triangle: "))
                 Height = int(input("Type height of your triangle: "))
@@ -82,6 +101,55 @@ def Calc():
                 TriangleCalc()
             elif(DirectPattern == 8):
                 RhombusCalc()
+            elif(DirectPattern == 9):
+                SideLength = int(input("Type side length of your rhombus: "))
+                RhoCirc = SideLength * 4
+                print("Circumference of your rhombus is {}".format(RhoCirc))
+            elif(DirectPattern == 10):
+                SideLength = int(input("Type side length of your parallelogram: "))
+                Height = int(input("Type height of your parallelogram: "))
+                ParArea = SideLength * Height
+                print("Area of your parallelogram is {}".format(ParArea))
+            elif(DirectPattern == 11):
+                SideLength1 = int(input("Type first side length of your parallelogram: "))
+                SideLength2 = int(input("Type second side length of your parallelogram: "))
+                ParCirc = (SideLength1 + SideLength2) * 2
+                print("Circumference of your parallelogram is {}".format(ParCirc))
+            elif(DirectPattern == 12):
+                BaseLength1 = int(input("Type first base length of your trapeze: "))
+                BaseLength2 = int(input("Type second base length of your trapeze: "))
+                Height = int(input("Type height of your trapeze: "))
+                TrapArea = (BaseLength1 + BaseLength2) * Height / 2
+            elif(DirectPattern == 13):
+                TrapezeCalc()
+            elif(DirectPattern == 14):
+                Radius = int(input("Type radius of your circle (half of a diameter): "))
+                CirlArea = round(Radius**2 * pi, 4)
+                print("Area of your circle is: {}π ({})".format(Radius**2,CirlArea))
+            elif(DirectPattern == 15):
+                Radius = int(input("Type radius of your circle (half of a diameter): "))
+                CirlCirc = round(Radius * pi * 2, 4)
+                print("Circumference of your circle is: {}π ({})".format(Radius * 2,CirlCirc))
+            elif(DirectPattern == 16):
+                EdgeLength = int(input("Type edge length of your cube: "))
+                CubeArea = EdgeLength**2 * 6
+                print("Area of your cube is: {}".format(CubeArea))
+            elif(DirectPattern == 17):
+                EdgeLength = int(input("Type edge length of your cube: "))
+                CubeVol = EdgeLength**3
+                print("Volume of your cube is: {}".format(CubeVol))
+            elif(DirectPattern == 18):
+                EdgeLength1 = int(input("Type first edge length of your cube: "))
+                EdgeLength2 = int(input("Type second edge length of your cube: "))
+                EdgeLength3 = int(input("Type third edge length of your cube: "))
+                CuboidArea = (EdgeLength1 * EdgeLength2 + EdgeLength1 * EdgeLength3 + EdgeLength2 * EdgeLength3) * 2
+                print("Area of your cuboid is: {}".format(CuboidArea))
+            elif(DirectPattern == 19):
+                EdgeLength1 = int(input("Type first edge length of your cube: "))
+                EdgeLength2 = int(input("Type second edge length of your cube: "))
+                EdgeLength3 = int(input("Type third edge length of your cube: "))
+                CuboidVol = EdgeLength1 * EdgeLength2 * EdgeLength3
+                print("Volume of your cuboid is: {}".format(CuboidVol))
             else:
                 print("This isn't currently available, but may be in future...")
                 Calc()
