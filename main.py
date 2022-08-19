21b0cb9ecc0171eb854ecca40f7563d67102f658
JacobPL53 <110395097+JacobPL53@users.noreply.github.com>
2022-08-19 10:49:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import math
 # Creating global variables: 'direct_pattern' - String and 'patterns' - Array
 direct_pattern = ""
 patterns = []
+# Definition of function - counts factorial of given integer 'b'
+def factorial():
+    number = input("Factorial of what number: ")
+    if(int(number) < 0):
+        print("Type number bigger than 0.")
+        factorial()
+    else:
+        a = 1
+        try:
+            number = int(number)
+        except ValueError:
+            print("Type number.")
+            factorial()
+        for i in range(1, number+1):
+            a *= i
+        print("Result of your factorial is: "+str(a))
+# Definition of function - counts circumference of any triangle
 def triangle_calc():
     triangle = str(input("Is your triangle equilateral or isosceles or different-sided?(e/i/d): "))
     if(triangle == "e" or triangle == "E"):
@@ -22,6 +39,7 @@ def triangle_calc():
     else:
         print("Type correct letter: e or i or d")
         triangle_calc()
+# Definition of function - counts area of rhombus with diagonals or with sides
 def rhombus_calc():
     diag_or_side = input("Formula with diagonals or with a side?(d/s)")
     if(diag_or_side.lower == "d"):
@@ -37,33 +55,34 @@ def rhombus_calc():
     else:
         print("Type correct letter: d or s")
         rhombus_calc()
-def TrapezeCalc():
-    IsIsosceles = input("Is your trapeze isosceles or not? (y/n): ")
-    if(IsIsosceles == "y" or IsIsosceles == "Y"):
-        BaseLength1 = int(input("Type first base length of your trapeze: "))
-        BaseLength2 = int(input("Type second base length of your trapeze: "))
-        TrapLeg = int(input("Type legs length in your trapeze: "))
-        TrapCirc = BaseLength1 + BaseLength2 + TrapLeg * 2
-    elif(IsIsosceles == "n" or IsIsosceles == "N"):
-        BaseLength1 = int(input("Type first base length of your trapeze: "))
-        BaseLength2 = int(input("Type second base length of your trapeze: "))
-        TrapLeg1 = int(input("Type first leg length in your trapeze: "))
-        TrapLeg2 = int(input("Type second leg length in your trapeze: "))
-        TrapCirc = BaseLength1 + BaseLength2 + TrapLeg1 + TrapLeg2
+# Definition of function - counts circumference of any trapeze
+def trapeze_calc():
+    is_isosceles = input("Is your trapeze isosceles or not? (y/n): ")
+    if(is_isosceles == "y" or is_isosceles == "Y"):
+        base_length1 = int(input("Type first base length of your trapeze: "))
+        base_length2 = int(input("Type second base length of your trapeze: "))
+        trap_leg = int(input("Type legs length in your trapeze: "))
+        trap_circ = base_length1 + base_length2 + trap_leg * 2
+    elif(is_isosceles == "n" or is_isosceles == "N"):
+        base_length1 = int(input("Type first base length of your trapeze: "))
+        base_length2 = int(input("Type second base length of your trapeze: "))
+        trap_leg1 = int(input("Type first leg length in your trapeze: "))
+        trap_leg2 = int(input("Type second leg length in your trapeze: "))
+        trap_circ = base_length1 + base_length2 + trap_leg1 + trap_leg2
     else:
         print("Type correct letter: y or n")
-        TrapezeCalc()
-    print("Circumference of your trapeze is: {}".format(TrapCirc))
-    
-def Calc():
+        trapeze_calc()
+    print("Circumference of your trapeze is: {}".format(trap_circ))
+
+def calc():
     # Ask for question branch
-    branchOfScience = int(input("Enter branch of your question: \nType 1 for Maths\nType 2 for Chemistry\nType 3 for Informatics\n:"))
-    if(branchOfScience == 1):
+    branch_of_science = int(input("Enter branch of your question: \nType 1 for Maths\nType 2 for Chemistry\nType 3 for Informatics\n:"))
+    if(branch_of_science == 1):
         categories = ["Basic Calculator","Geometry"]
         category = int(input("1 - {}\n2 - {}\nSelect one of those: ".format(*categories)))
         if(category == 1):
-            patterns = ["Addition","Subtraction","Multiplication","Division","Power","Root","Modulo"]
-            print("\n1 - {}\n2 - {}\n3 - {}\n4 - {}\n5 - {}\n6 - {}\n7 - {}".format(*patterns))
+            patterns = ["Addition","Subtraction","Multiplication","Division","Power","Root","Modulo","Factorial"]
+            print("\n1 - {}\n2 - {}\n3 - {}\n4 - {}\n5 - {}\n6 - {}\n7 - {}\n8 - {}".format(*patterns))
             direct_pattern = int(input("Select one of above: "))
             if(direct_pattern == 1):
                 ingredient1 = int(input("Type first ingredient: "))
@@ -90,118 +109,120 @@ def Calc():
                     print(f"Quotient of those numbers is: {quotient} ({fullquotient} and {remainder} remainder)")
                 else:
                     print(f"Quotient of those numbers is: {quotient}")
+            elif(direct_pattern == 8):
+                factorial()
         elif(category == 2):
             patterns = ["Square Diagonal","Square Area","Square Circumference","Rectangle Area","Rectangle Circumference","Triangle Area","Triangle Circumference","Rhombus Area","Rhombus Circumference","Parallelogram Area","Parallelogram Circumference","Trapeze Area","Trapeze Circumference","Circle Area","Circle Circumference","Cube Area","Cube Volume","Cuboid Area","Cuboid Volume","Cylinder Area","Cylinder Volume","Sphere Area","Sphere Volume"]
             print("\n 1 - {}\n 2 - {}\n 3 - {}\n 4 - {}\n 5 - {}\n 6 - {}\n 7 - {}\n 8 - {}\n 9 - {}\n10 - {}\n11 - {}\n12 - {}\n13 - {}\n14 - {}\n15 - {}\n16 - {}\n17 - {}\n18 - {}\n19 - {}\n20 - {}\n21 - {}\n22 - {}\n23 - {}\n".format(*patterns))
             direct_pattern = int(input("Select one of above: "))
             if(direct_pattern == 1):
                 side_length = int(input("Type side length of your square: "))
-                Diagonal = round(side_length * math.sqrt(2),3)
-                print("Diagonal of your square is: {}√2 ({})".format(side_length, Diagonal))
+                diagonal = round(side_length * math.sqrt(2),3)
+                print("Diagonal of your square is: {}√2 ({})".format(side_length, diagonal))
             elif(direct_pattern == 2):
                 side_length = int(input("Type side lenght of your square: "))
-                SquareArea = side_length ** 2
-                print("Area of your square is: {}".format(SquareArea))
+                square_area = side_length ** 2
+                print("Area of your square is: {}".format(square_area))
             elif(direct_pattern == 3):
                 side_length = int(input("Type side length of your square: "))
-                SquareCirc = side_length * 4
-                print("Circumference of your square is: {}".format(SquareCirc))
+                square_circ = side_length * 4
+                print("Circumference of your square is: {}".format(square_circ))
             elif(direct_pattern == 4):
-                RectWidth = int(input("Type width of your rectangle: "))
-                Rectheight = int(input("Type height of your rectangle: "))
-                RectArea = Rectheight * RectWidth
-                print("Area of your rectangle is: {}".format(RectArea))
+                rect_width = int(input("Type width of your rectangle: "))
+                rect_height = int(input("Type height of your rectangle: "))
+                rect_area = rect_height * rect_width
+                print("Area of your rectangle is: {}".format(rect_area))
             elif(direct_pattern == 5):
-                RectWidth = int(input("Type width of your rectangle: "))
-                Rectheight = int(input("Type height of your rectangle: "))
-                RectCirc = 2 * RectWidth + 2 * Rectheight
-                print("Circumference of your rectangle is: {}".format(RectCirc))
+                rect_width = int(input("Type width of your rectangle: "))
+                rect_height = int(input("Type height of your rectangle: "))
+                rect_circ = 2 * rect_width + 2 * rect_height
+                print("Circumference of your rectangle is: {}".format(rect_circ))
             elif(direct_pattern == 6):
                 side_length = int(input("Type side length of your triangle: "))
                 height = int(input("Type height of your triangle: "))
-                TriArea = side_length * height / 2
-                print("Area of your triangle is {}".format(TriArea))
+                tri_area = side_length * height / 2
+                print("Area of your triangle is {}".format(tri_area))
             elif(direct_pattern == 7):
                 triangle_calc()
             elif(direct_pattern == 8):
                 rhombus_calc()
             elif(direct_pattern == 9):
                 side_length = int(input("Type side length of your rhombus: "))
-                RhoCirc = side_length * 4
-                print("Circumference of your rhombus is {}".format(RhoCirc))
+                rho_circ = side_length * 4
+                print("Circumference of your rhombus is {}".format(rho_circ))
             elif(direct_pattern == 10):
                 side_length = int(input("Type side length of your parallelogram: "))
                 height = int(input("Type height of your parallelogram: "))
-                ParArea = side_length * height
-                print("Area of your parallelogram is {}".format(ParArea))
+                par_area = side_length * height
+                print("Area of your parallelogram is {}".format(par_area))
             elif(direct_pattern == 11):
                 side_length1 = int(input("Type first side length of your parallelogram: "))
                 side_length2 = int(input("Type second side length of your parallelogram: "))
-                ParCirc = (side_length1 + side_length2) * 2
-                print("Circumference of your parallelogram is {}".format(ParCirc))
+                par_circ = (side_length1 + side_length2) * 2
+                print("Circumference of your parallelogram is {}".format(par_circ))
             elif(direct_pattern == 12):
-                BaseLength1 = int(input("Type first base length of your trapeze: "))
-                BaseLength2 = int(input("Type second base length of your trapeze: "))
+                base_length1 = int(input("Type first base length of your trapeze: "))
+                base_length2 = int(input("Type second base length of your trapeze: "))
                 height = int(input("Type height of your trapeze: "))
-                TrapArea = (BaseLength1 + BaseLength2) * height / 2
+                trap_area = (base_length1 + base_length2) * height / 2
             elif(direct_pattern == 13):
-                TrapezeCalc()
+                trapeze_calc()
             elif(direct_pattern == 14):
-                Radius = int(input("Type radius of your circle (half of a diameter): "))
-                CirlArea = round(Radius**2 * math.pi, 4)
-                print("Area of your circle is: {}π ({})".format(Radius**2,CirlArea))
+                radius = int(input("Type radius of your circle (half of a diameter): "))
+                cirl_area = round(radius**2 * math.pi, 4)
+                print("Area of your circle is: {}π ({})".format(radius**2,cirl_area))
             elif(direct_pattern == 15):
-                Radius = int(input("Type radius of your circle (half of a diameter): "))
-                CirlCirc = round(Radius * math.pi * 2, 4)
-                print("Circumference of your circle is: {}π ({})".format(Radius * 2,CirlCirc))
+                radius = int(input("Type radius of your circle (half of a diameter): "))
+                cirl_circ = round(radius * math.pi * 2, 4)
+                print("Circumference of your circle is: {}π ({})".format(radius * 2,cirl_circ))
             elif(direct_pattern == 16):
-                EdgeLength = int(input("Type edge length of your cube: "))
-                CubeArea = EdgeLength**2 * 6
-                print("Area of your cube is: {}".format(CubeArea))
+                edge_length = int(input("Type edge length of your cube: "))
+                cube_area = edge_length**2 * 6
+                print("Area of your cube is: {}".format(cube_area))
             elif(direct_pattern == 17):
-                EdgeLength = int(input("Type edge length of your cube: "))
-                CubeVol = EdgeLength**3
-                print("Volume of your cube is: {}".format(CubeVol))
+                edge_length = int(input("Type edge length of your cube: "))
+                cube_vol = edge_length**3
+                print("Volume of your cube is: {}".format(cube_vol))
             elif(direct_pattern == 18):
-                EdgeLength1 = int(input("Type first edge length of your cube: "))
-                EdgeLength2 = int(input("Type second edge length of your cube: "))
-                EdgeLength3 = int(input("Type third edge length of your cube: "))
-                CuboidArea = (EdgeLength1 * EdgeLength2 + EdgeLength1 * EdgeLength3 + EdgeLength2 * EdgeLength3) * 2
-                print("Area of your cuboid is: {}".format(CuboidArea))
+                edge_length1 = int(input("Type first edge length of your cube: "))
+                edge_length2 = int(input("Type second edge length of your cube: "))
+                edge_length3 = int(input("Type third edge length of your cube: "))
+                cuboid_area = (edge_length1 * edge_length2 + edge_length1 * edge_length3 + edge_length2 * edge_length3) * 2
+                print("Area of your cuboid is: {}".format(cuboid_area))
             elif(direct_pattern == 19):
-                EdgeLength1 = int(input("Type first edge length of your cube: "))
-                EdgeLength2 = int(input("Type second edge length of your cube: "))
-                EdgeLength3 = int(input("Type third edge length of your cube: "))
-                CuboidVol = EdgeLength1 * EdgeLength2 * EdgeLength3
-                print("Volume of your cuboid is: {}".format(CuboidVol))
+                edge_length1 = int(input("Type first edge length of your cube: "))
+                edge_length2 = int(input("Type second edge length of your cube: "))
+                edge_length3 = int(input("Type third edge length of your cube: "))
+                cuboid_vol = edge_length1 * edge_length2 * edge_length3
+                print("Volume of your cuboid is: {}".format(cuboid_vol))
             elif(direct_pattern == 20):
-                Radius = int(input("Type radius of your cylinder (half of a diameter): "))
+                radius = int(input("Type radius of your cylinder (half of a diameter): "))
                 height = int(input("Type height of your cyliner: "))
-                CylArea = math.pi * Radius * (Radius + height) * 2
-                print("Area of your cylinder is: {}π ({})".format(Radius*(Radius+height)*2,round(CylArea)))
+                cyl_area = math.pi * radius * (radius + height) * 2
+                print("Area of your cylinder is: {}π ({})".format(radius*(radius+height)*2,round(cyl_area)))
             elif(direct_pattern == 21):
-                Radius = int(input("Type radius of your cylinder (half of a diameter): "))
+                radius = int(input("Type radius of your cylinder (half of a diameter): "))
                 height = int(input("Type height of your cyliner: "))
-                CylVol = math.pi * Radius**2 * height
-                print("Volume of your cylinder is: {}π ({})".format(Radius**2*height,round(CylVol,4)))
+                cyl_vol = math.pi * radius**2 * height
+                print("Volume of your cylinder is: {}π ({})".format(radius**2*height,round(cyl_vol,4)))
             elif(direct_pattern == 22):
-                Radius = int(input("Type radius of your cylinder (half of a diameter): "))
-                SphArea = math.pi * Radius**2 * 4
-                print("Area of your sphere is: {}π ({})".format(Radius**2*4,round(SphArea,4)))
+                radius = int(input("Type radius of your cylinder (half of a diameter): "))
+                sph_area = math.pi * radius**2 * 4
+                print("Area of your sphere is: {}π ({})".format(radius**2*4,round(sph_area,4)))
             elif(direct_pattern == 23):
-                Radius = int(input("Type radius of your cylinder (half of a diameter): "))
-                SphVol = math.pi * Radius**3 * 4 / 3
-                print("Volume of your sphere is: {}π ({})".format(Radius**3*4/3,round(SphVol,4)))
+                radius = int(input("Type radius of your cylinder (half of a diameter): "))
+                sph_vol = math.pi * radius**3 * 4 / 3
+                print("Volume of your sphere is: {}π ({})".format(radius**3*4/3,round(sph_vol,4)))
             else:
                 print("This isn't currently available, but may be in future...")
-                Calc()
-    elif(branchOfScience == 2):
+                calc()
+    elif(branch_of_science == 2):
         patterns = ["Density","Volume","Mass"]
         direct_pattern = int(input("Select one of those: \n1 - {}\n2 - {}\n3 - {}\n".format(*patterns)))
-    elif(branchOfScience == 3):
+    elif(branch_of_science == 3):
         patterns = ["Byte Calculator"]
         direct_pattern = int(input("Select one of those: \n1 - {}\n".format(*patterns)))
     else:
        print("Type correct number")
-       Calc()
-Calc()
+       calc()
+calc()
